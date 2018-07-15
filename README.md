b9a80335b34774eb4266389b295f247605963a85
varan <varan@VARAN-PC>
2018-07-15 14:58:57 +0300

    add script and readme

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+---
+This script show duplicate files in directory
+---
+
+### Discription
++ Specify the path to start
++ This script show coinciding by name and size files
+
+### How starting
+```bash
+python dublicate.py <your directory>
+```
+
+### Example result
+```bash
+C:\Users\varan\PycharmProjects\firstproject\venv\Lib\site-packages\PyQt5\Qt\qml\QtWebEngine\Controls1Delegates\MenuItem.qml
+C:\Users\varan\PycharmProjects\firstproject\venv\Lib\site-packages\PyQt5\Qt\qml\QtWebEngine\Controls2Delegates\MenuItem.qml
+
+
+C:\Users\varan\PycharmProjects\firstproject\venv\Lib\site-packages\PyQt5\uic\Compiler\__init__.py
+C:\Users\varan\PycharmProjects\firstproject\venv\Lib\site-packages\PyQt5\uic\Loader\__init__.py
+C:\Users\varan\PycharmProjects\firstproject\venv\Lib\site-packages\PyQt5\uic\port_v2\__init__.py
+C:\Users\varan\PycharmProjects\firstproject\venv\Lib\site-packages\PyQt5\uic\port_v3\__init__.py
+```
+
+### Requirements
+```bash
+Python ver 3.5 (or higher)
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,46 @@
+import os
+import argparse
+
+
+def search_for_duplicate_files(path):
+    name_files = {}
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            size_file = os.path.getsize(root+"\\"+file)
+            if (file, size_file) in name_files:
+                name_files[(file, size_file)].append(root)
+            else:
+                name_files[(file, size_file)] = [root]
+    duplicate_files = [
+        [path+"\\"+duplicates[0] for path in name_files[duplicates]]
+        for duplicates in name_files if len(name_files[duplicates]) > 1
+    ]
+    return duplicate_files
+
+
+def pprint_duplicate_files(duplicates_files):
+    for duplicates in duplicates_files:
+        for file in duplicates:
+            print(file)
+        print("\n")
+
+
+def get_parser_args():
+    parser = argparse.ArgumentParser(
+        description="Path to the check for duplication"
+    )
+    parser.add_argument(
+        "path",
+        help="input path to check"
+    )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+    arguments = get_parser_args()
+
+    if os.path.isdir(arguments.path):
+        duplicates = search_for_duplicate_files(arguments.path)
+        pprint_duplicate_files(duplicates)
+    else:
+        print("Error: This is a file or directory not found!")
